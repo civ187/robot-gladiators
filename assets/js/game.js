87d4ee7bcbb99328a1db25bf1b88be2decab7a44
Victor Cesar Lopez <civ187@gmail.com>
2020-06-01 22:33:33 -0500

    3.1.5 Commit

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,22 @@
-window.alert("This is an Alert! JavaScript is running!");
+// this creates a function named "fight"
+
+function fight () {
+    window.alert("The fight has begun!")
+}
+
+var playerName = window.prompt ("What is your robot's name?")
+
+//window.alert (playerName) ;
+console.log(playerName);
+console.log("this logs a string, good for leaving yourself a message");
+console.log(10+10);
+console.log("Our robot's name is "+ playerName);
+console.log()
+
+
+
+
+
+//fight();
+
+
