13ea8ccce69eb58c706c1c3342091abaee4cfbfe
Victor Cesar Lopez <civ187@gmail.com>
2020-06-04 18:59:11 -0500

    add the shop funciton

--- assets/js/game.js
+++ assets/js/game.js
@@ -95,13 +95,23 @@ var startGame = function() {
         
         // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
         fight(pickedEnemyName);
+
+        if (playerHealth > 0 && i < enemyNames.length - 1) {
+          //ask if user want to use the store before next round
+          
+          var storeConfirm = window.confirm("The fight is over, visit the store before the next round");
+          //if yes, take them to the store() function
+          
+          if (storeConfirm) {
+            shop();
         }
+      }
         else {
             window.alert("You have lost your robot in battle! GAME OVER!");
             break;
         }
+      }
     }
-
   endGame();  
 };
 
@@ -124,9 +134,14 @@ var endGame = function(){
   else {
     window.alert("Thank you for playing Robot Gladiators! Come back soon!");
   }
-}
+};
 
-
+var shop = function(){
+  //ask player what they'd like to do
+  var shopOptionPrompt = window.prompt (
+    "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+  );
+};
 
 
 //start the game when the page loads
